990622afee7f472a4c71079e5b60155fe7ca1065
Chris McDonough <chrism@plope.com>
2011-07-02 02:16:44 -0400

    use os.path.join

--- deform_mako/__init__.py
+++ deform_mako/__init__.py
@@ -1,8 +1,10 @@
+import os
 from mako.template import Template
 
 def mako_renderer_factory(directory, translator=None):
     def mako_renderer(tname, **kw):
-        template = Template(filename='%s%s.mako' % (directory, tname))
+        filename = os.path.join(directory, tname) + '.mako'
+        template = Template(filename=filename)
         return template.render(_=translator, **kw)
     return mako_renderer
 
